cdbba8377f5a4e18e0c0c0f74a40c4a6edd60f5b
Alexandre Gutierry <aleguti1000@hotmail.com>
2022-07-22 10:53:36 -0300

    navegacao prev e next finalizada

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-import Slide from './slide.js'
-const slide = new Slide('.slide', '.slide-wrapper')
+import { SlideNav } from './slide.js'
+
+const slide = new SlideNav('.slide', '.slide-wrapper')
 slide.init()
-slide.changeSlide(3)
+slide.addArrow('.prev', '.next')
+console.log(slide)
+// slide.changeSlide(3)
